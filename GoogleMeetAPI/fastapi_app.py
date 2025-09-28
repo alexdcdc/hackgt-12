8b6eca2117acf79b4bca095cac8a09067c08e97d
William Wu <williamwu@Williams-MacBook-Air.local>
2025-09-28 03:43:44 -0400

    webhook integration & video downloading for analysis

--- GoogleMeetAPI/fastapi_app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi import Request
-from fastapi.responses import JSONResponse
-
-app = FastAPI()
-
-@app.get("/hello")
-async def hello_world(request: Request):
-    return JSONResponse(content={"message": "Hello, world!"})
